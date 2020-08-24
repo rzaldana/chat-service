9f895b7e65d363bb3861432f44c8ec75c79dadc5
Raul Zaldana <raularmandozc12@gmail.com>
2020-08-24 19:16:20 -0400

    created joinChat function

--- src/joinChat.js
+++ src/joinChat.js
@@ -1,14 +1,59 @@
+import AWS from "aws-sdk";
 
-export const handler = async event => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'You joined a chat!',
-            },
-            null,
-            2
-        ),
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export const handler = async (event, context) => {
+    // get connectionId, chatId and alias
+    const connectionId = event.requestContext.connectionId;
+    const body = JSON.parse(event.body);
+    const chatId = body.chatId;
+    const alias = body.alias;
+
+    // get chat record from chatsTable
+    try {
+        let params = {
+            TableName: process.env.CHATS_TABLE_NAME,
+            Key: { "id": chatId }
+        };
+        let result = await dynamodb.get(params).promise();
+        var chat = result.Item;
+    } catch (error) {
+        console.log("found error when getting chat record from chatsTable");
+        console.error(error);
+    }
+
+    // add user to connectionsTable
+    const connection = {
+        id: connectionId,
+        alias
     };
+
+    try {
+        let params = {
+            TableName: process.env.CONNECTIONS_TABLE_NAME,
+            Item: connection
+        };
+        await dynamodb.put(params).promise();
+    } catch (error) {
+        console.log("found error when adding user to connectionsTable");
+        console.error(error);
+    }
+
+    // add user to participants of chat in ChatsTable
+    try {
+        let params = {
+            TableName: process.env.CHATS_TABLE_NAME,
+            Key: { id: chatId },
+            UpdateExpression: `SET participants[${chat.participants.length}] = :c`,
+            ConditionExpression: `size(participants) = :le`,
+            ExpressionAttributeValues: { ':c': connectionId, ':le': chat.participants.length }
+        };
+        await dynamodb.update(params).promise();
+    } catch (error) {
+        console.log("found error when adding user to participants in ChatsTable");
+        console.error(error);
+    }
+
+    return {};
 
 };
